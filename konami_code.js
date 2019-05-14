84dcae30144bc331035ca3ebf95578f091d47c8a
github username <github email address>
2019-05-14 15:57:01 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,19 +13,14 @@ const codes = [
 
 function init() {
   // your code here
-  const alphabet = ['a', 'b', 'c'];
-
 // Keep track of index outside of the event handler.
 let index = 0;
-
-// This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
+document.body.addEventListener("keydown", e=>{
   const key = e.key;
-
-  if (key === alphabet[index]) {
+  if (key === codes[index]) {
     index++;
 
-    if (index === alphabet.length) {
+    if (index === codes.length) {
       alert("Hurray!");
 
       index = 0;
@@ -33,6 +28,5 @@ function onKeyDownHandler(e) {
   } else {
     index = 0;
   }
-}
-
+})
 }
